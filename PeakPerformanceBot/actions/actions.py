8f8d33b6ae3c3ff2f8e7c065a356781a9085481b
Tomaicho <tomas.a.lima@gmail.com>
2023-05-28 03:55:38 +0100

    Já temos bicho
    Co-authored-by: Ciaran McEvoy <ciaranmcevoy07@users.noreply.github.com>

--- PeakPerformanceBot/actions/actions.py
+++ PeakPerformanceBot/actions/actions.py
@@ -25,7 +25,7 @@ class calculateTrailClimb(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         distance = next(tracker.get_latest_entity_values("goaldistance"), None)
         uphill = tracker.get_slot("ascent")
-        downhill = tracker.get_slot("descent")
+        # downhill = tracker.get_slot("descent")
         distance_unit = next(tracker.get_latest_entity_values("goaldistanceunit"), None)
 
         if not uphill:
@@ -130,3 +130,45 @@ class convertDistance(Action):
         
         finalmetres = (km*1000) + m 
         return[SlotSet("goaldistancefinal", finalmetres)]
+
+class convertTimeLeft(Action):
+    def name(self) -> Text:
+        return "convert_time_left"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        tl_days = tracker.get_slot("time_left_days")
+        tl_years = tracker.get_slot("time_left_years")
+        tl_months = tracker.get_slot("time_left_months")
+        tl_weeks = tracker.get_slot("time_left_weeks")
+
+        if tl_days != None:
+            match = re.search(r'[0-9]+', tl_days)
+            if match:
+                days = int(match.group())
+        else:
+            days = 0
+        if tl_months != None:
+            match = re.search(r'[0-9]+', tl_months)
+            if match:
+                months = int(match.group())
+        else:
+            months = 0
+        if tl_years != None:
+            match = re.search(r'[0-9]+', tl_years)
+            if match:
+                years = int(match.group())
+        else:
+            years = 0
+        if tl_weeks != None:
+            match = re.search(r'[0-9]+', tl_weeks)
+            if match:
+                weeks = int(match.group())
+        else:
+            weeks = 0
+        
+        finaldays = (years*365) + (months*30) + (weeks*7) + days
+        finalweeks = finaldays//7
+        return[SlotSet("time_left_final", finalweeks)]
