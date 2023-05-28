13059ae3a1706311ccef324dc9231fbb643ba1c0
Tomaicho <tomas.a.lima@gmail.com>
2023-05-28 01:15:38 +0100

    sei lá
    Co-authored-by: GoncaloCar <GoncaloCar@users.noreply.github.com>
    Co-authored-by: Ciaran McEvoy <ciaranmcevoy07@users.noreply.github.com>

--- PeakPerformanceBot/actions/actions.py
+++ PeakPerformanceBot/actions/actions.py
@@ -9,9 +9,10 @@
 
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+import re
 
 
 class calculateTrailClimb(Action):
@@ -47,28 +48,85 @@ class calculateTrailClimb(Action):
             dispatcher.utter_message(response = "utter_small_climb")
         return []
 
-class checkInfo(Action):
+# class ValidateRaceInfoForm(FormValidationAction):
+#     def name(self) -> Text:
+#         return "validate_race_info_form"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         goal_hours = tracker.get_slot("goalhours")
+#         goal_minutes = tracker.get_slot("goalminutes")
+#         goal_seconds = tracker.get_slot("goalseconds") 
+
+        
+#     def validate_goaltime(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:   
+        
+        
+        
+#         return {"goalhours":slot_value}
+    
+
+class convertTime(Action):
     def name(self) -> Text:
-        return "check_info"
+        return "convert_time"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
         goal_hours = tracker.get_slot("goalhours")
         goal_minutes = tracker.get_slot("goalminutes")
-        goal_seconds = tracker.get_slot("goalseconds") 
+        goal_seconds = tracker.get_slot("goalseconds")
 
-        if goal_hours is not None or goal_minutes is not None or goal_seconds is not None:
-            dispatcher.utter_message(text=f"Thank you")
+        if goal_hours != None:
+            match = re.search(r'[0-9]+', goal_hours)
+            if match:
+                hours = int(match.group())
         else:
-            dispatcher.utter_message(text=f"Please provide the time.")
-            goalhours = next(tracker.get_latest_entity_values("goalhours"), None)
-            goalminutes = next(tracker.get_latest_entity_values("goalminutes"), None)
-            goalseconds = next(tracker.get_latest_entity_values("goalseconds"), None)
-            if goalhours != None:
-                SlotSet("goalhours", goalhours)
-            if goalminutes != None:
-                SlotSet("goalminutes", goalminutes)
-            if goalminutes != None:
-                SlotSet("goalseconds", goalseconds)
-        return []
+            hours = 0
+        if goal_minutes != None:
+            match = re.search(r'[0-9]+', goal_minutes)
+            if match:
+                minutes = int(match.group())
+        else:
+            minutes = 0
+        if goal_seconds != None:
+            match = re.search(r'[0-9]+', goal_seconds)
+            if match:
+                seconds = int(match.group())
+        else:
+            seconds = 0
+        
+        finalseconds = (hours*3600) + (minutes*60) + seconds
+        return[SlotSet("goaltimefinal", finalseconds)]
+
+class convertDistance(Action):
+    def name(self) -> Text:
+        return "convert_distance"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        goaldistance_km = tracker.get_slot("goaldistance_km")
+        goaldistance_m = tracker.get_slot("goaldistance_m")
+        
+
+        if goaldistance_km != None:
+            match = re.search(r'[0-9]+', goaldistance_km)
+            if match:
+                km = int(match.group())
+        else:
+            km = 0
+        if goaldistance_m != None:
+            match = re.search(r'[0-9]+', goaldistance_m)
+            if match:
+                m = int(match.group())
+        else:
+            m = 0
+        
+        finalmetres = (km*1000) + m 
+        return[SlotSet("goaldistancefinal", finalmetres)]
